00c1c4afadbe29de6a98e5641498cec46e70b9cf
Mateo Estrada <mateoej12@gmail.com>
2021-12-08 15:30:46 -0800

    Variables in Rust.

---
+++ variables/target/debug/deps/variables-425e8b81a797b7b8.d
@@ -0,0 +1,5 @@
+/Users/mateog.estrada/rusts/variables/target/debug/deps/variables-425e8b81a797b7b8: src/main.rs
+
+/Users/mateog.estrada/rusts/variables/target/debug/deps/variables-425e8b81a797b7b8.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/mateog.estrada/rusts/variables/target/debug/variables: /Users/mateog.estrada/rusts/variables/src/main.rs
