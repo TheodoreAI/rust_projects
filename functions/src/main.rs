b1b2cdeb1fad235b166ef2e7bc18a61ae91b97ac
Mateo Estrada <mateoej12@gmail.com>
2021-12-08 16:19:21 -0800

    functions in Rust.

---
+++ functions/src/main.rs
@@ -0,0 +1,24 @@
+fn main() {
+    another_function(5, 9);
+    let x = five();
+    println!("The return value of x is: {}", x);
+
+    let y = plus_one(5);
+    println!("The value of plus_one is: {}", y);
+}
+
+fn five() -> u32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32{
+    x + 1
+}
+
+fn another_function(x: u32, y: u32){
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
+
+   
+   
+}
