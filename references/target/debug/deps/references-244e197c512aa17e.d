921b290861be4be7be0683a997dd6d559919605c
Mateo <mateoej12@gmail.com>
2022-09-19 09:31:14 -0700

    Adding notes.

---
+++ references/target/debug/deps/references-244e197c512aa17e.d
@@ -0,0 +1,5 @@
+/Users/mateoestrada/Documents/rust/rust_projects/references/target/debug/deps/references-244e197c512aa17e: src/main.rs
+
+/Users/mateoestrada/Documents/rust/rust_projects/references/target/debug/deps/references-244e197c512aa17e.d: src/main.rs
+
+src/main.rs:
---
+++ structs/target/debug/structs.d
@@ -0,0 +1,1 @@
+/Users/mateoestrada/Documents/rust/rust_projects/structs/target/debug/structs: /Users/mateoestrada/Documents/rust/rust_projects/structs/src/main.rs
